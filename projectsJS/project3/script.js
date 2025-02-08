920e89e56bee460c0a5e674989a2ab602d1ab647
Gabrielle Bassiquete Silva <100743446+gabrielle978@users.noreply.github.com>
2025-02-07 22:50:55 -0300

    Add files via upload

---
+++ projectsJS/project3/script.js
@@ -0,0 +1,9 @@
+const controls = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing || ''; 
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+controls.forEach(input => input.addEventListener('change', handleUpdate));
+controls.forEach(input => input.addEventListener('mousemove', handleUpdate));
